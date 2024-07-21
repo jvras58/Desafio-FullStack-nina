708de6bf5917a4b699c5186f85a7e8304a7e10cb
Jonathas Vinicius <jonathas.vinicius@ufpe.br>
2024-07-21 06:54:55 -0300

    feat: Add complaint detail component and route

--- front/src/app/app.routes.ts
+++ front/src/app/app.routes.ts
@@ -1,7 +1,9 @@
 import { Routes } from '@angular/router';
+import { ComplaintDetailComponent } from './components/complaint-detail/complaint-detail.component';
 import { TabelaComponent } from './components/tabela/tabela.component';
 
 export const routes: Routes = [
     { path: '', component: TabelaComponent },
+    { path: 'complaints/:id', component: ComplaintDetailComponent },
 
 ];
